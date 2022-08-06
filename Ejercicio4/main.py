260f2c9b4aa22b299ed6c7ca491702e3d4252fef
juancalderonx <juanescalderon12@gmail.com>
2022-08-06 17:29:07 -0500

    4. Exercise

---
+++ Ejercicio4/main.py
@@ -0,0 +1,24 @@
+name = 'Juan'
+lastName = 'Calderón'
+age = 19
+email = 'juan@calderon.com'
+cellphone = 3100000000
+married = False
+withChildren = False
+friends = ['Camilo', 'Lola', 'Gabo']
+movies = {
+    1: 'Harry Potter',
+    2: 'Game of Thrones',
+    3: 'Rings of Power'
+}
+
+print('My data is:')
+print('Name: ' + name)
+print('Last name: ' + lastName)
+print('Age: ' + str(age))
+print('Email: ' + email)
+print('Cellphone: ' + str(cellphone))
+print('Married?: ' + str(married))
+print("Do you've children?: " + str(withChildren))
+print('Friends: ' + str(friends))
+print('Movies: ' + str(movies))
